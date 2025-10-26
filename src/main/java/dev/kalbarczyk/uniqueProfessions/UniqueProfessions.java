f8183f77a87219708a88d8c3e08cfef92c43f235
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2025-10-26 10:42:21 +0100

    initialize UniqueProfessions plugin with command and event listeners

--- src/main/java/dev/kalbarczyk/uniqueProfessions/UniqueProfessions.java
+++ src/main/java/dev/kalbarczyk/uniqueProfessions/UniqueProfessions.java
@@ -1,17 +1,57 @@
 package dev.kalbarczyk.uniqueProfessions;
 
+import dev.kalbarczyk.uniqueProfessions.commands.AdminCommand;
+import dev.kalbarczyk.uniqueProfessions.commands.ProfessionCommand;
+import dev.kalbarczyk.uniqueProfessions.listeners.BlockListener;
+import dev.kalbarczyk.uniqueProfessions.listeners.ItemListener;
+import dev.kalbarczyk.uniqueProfessions.listeners.PlayerListener;
+import dev.kalbarczyk.uniqueProfessions.player.PlayerDataManager;
+import dev.kalbarczyk.uniqueProfessions.profession.ProfessionManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 public final class UniqueProfessions extends JavaPlugin {
 
+    private static UniqueProfessions instance;
+    private PlayerDataManager playerDataManager;
+    private ProfessionManager professionManager;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
+        playerDataManager = new PlayerDataManager(this);
+        professionManager = new ProfessionManager(this);
+
+        getServer().getPluginManager().registerEvents(new BlockListener(this), this);
+        getServer().getPluginManager().registerEvents(new ItemListener(this), this);
+        getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
+
+        Objects.requireNonNull(getCommand("profession")).setExecutor(new ProfessionCommand(this));
+        Objects.requireNonNull(getCommand("professionadmin")).setExecutor(new AdminCommand(this));
+
+        getLogger().info("ProfessionPlugin has been enabled!");
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        if (playerDataManager != null) {
+            playerDataManager.saveAll();
+        }
+
+        getLogger().info("ProfessionPlugin has been disabled!");
+    }
+
+    public static UniqueProfessions getInstance() {
+        return instance;
+    }
+
+    public PlayerDataManager getPlayerDataManager() {
+        return playerDataManager;
+    }
+
+    public ProfessionManager getProfessionManager() {
+        return professionManager;
     }
 }
